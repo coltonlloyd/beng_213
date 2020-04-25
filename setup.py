36439f3640385198526c2032e8bdca4fa5bba4a0
coltonlloyd <cjlloyd@ucsd.edu>
2020-04-25 16:04:51 -0700

    simplify setup

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(name="beng213",
       version="",
       author="Colton Lloyd",
-      url="https://github.com/coltonlloyd/qbio",
+      url="https://github.com/coltonlloyd/beng_213",
       packages=find_packages(),
-      install_requires=["six", "cobra", "pandas", "scipy", "sklearn",
-                        "setuptools", "graphviz", "pydotplus", "seaborn",
+      install_requires=["six", "cobra", "pandas", "scipy",
+                        "setuptools",  "seaborn",
                         "matplotlib", "escher"],
       )
